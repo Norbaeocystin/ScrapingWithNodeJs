9cd4f0f44ae1d69626093e462e206bf2cbe70524
Norbaeocystin <baranrastislav@gmail.com>
2020-09-05 23:08:47 +0200

    Update install.sh

--- ExpressWithPuppeteer/install.sh
+++ ExpressWithPuppeteer/install.sh
@@ -3,11 +3,13 @@
 # 'http://<server ip>:3000/api?url=http://topky.sk'
 yes | apt update
 yes | apt install nodejs
+yes | apt install chromium-browser
 yes | apt install npm
 yes | apt install screen
 yes | apt-get install gconf-service libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxss1 libxtst6 libappindicator1 libnss3 libasound2 libatk1.0-0 libc6 ca-certificates fonts-liberation lsb-release xdg-utils wget
 npm install express --save
-npm install puppeteer --unsafe-perm
+#npm install puppeteer --unsafe-perm
+npm install puppeteer-core
 npm install puppeteer-extra
 npm install puppeteer-extra-plugin-stealth
 cat <<EOT >> index.js
@@ -29,7 +31,8 @@ async function get_html(url){
 '--ignore-certifcate-errors-spki-list',
 '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
 ],
-    headless: true
+    headless: true,
+    executablePath: '/usr/bin/chromium-browser'
   })
   const page = await browser.newPage()
  
